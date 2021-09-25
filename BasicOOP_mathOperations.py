06444a1c76bae85dfb89acf9b044aec778f5dc22
Rashaan-Richardson <73082367+Rashaan-Richardson@users.noreply.github.com>
2021-09-25 00:33:28 -0400

    Update BasicOOP_mathOperations.py

--- BasicOOP_mathOperations.py
+++ BasicOOP_mathOperations.py
@@ -1,4 +1,8 @@
+import logging
 import math
+
+# Logging statment. "basic config(filename = path /what you want to call your logger",level = logging level)
+logging.basicConfig(filename='/Users/rashaanrichardson/Desktop/PY_121_Class/Basic_Math_Logger',level=logging.INFO)
 
 class Numbers:
     def __init__(self,num1,num2):
@@ -6,29 +10,31 @@ class Numbers:
         self.num2 = num2
 
     def add_num(self):
-        return f'The sum of these numbers is {self.num1 + self.num2}'
+        logging.info(f'The sum of these numbers is {self.num1 + self.num2}')
 
     def sub_num(self):
-        return f'The two numbers subtracted equals {self.num1 - self.num2}'
+        logging.info(f'The two numbers subtracted equals {self.num1 - self.num2}')
 
     def multi(self):
-        return f'The product of these numbers is {self.num1 * self.num2}'
+        logging.info(f'The product of these numbers is {self.num1 * self.num2}')
 
     def divide(self):
-        return f'The result of the division is {self.num1/self.num2}'
+        logging.info(f'The result of the division is {self.num1/self.num2}')
 
-    def findPrime(self, num):
-        if(num > 1):
+    def findPrime(self):
+        if(self.num1 > 1):
             prime = True
-            for i in range(2, int(math.sqrt(num) + 1)):
-                if num % i == 0:
+            for i in range(2, int(math.sqrt(self.num1) + 1)):
+                if self.num1 % i == 0:
                     prime = False
                     break
-        return 'Prime' if prime else 'Composite'
+        logging.info('Prime' if prime else 'Composite')
 
 if  __name__ == '__main__':
+    
     num_operations = Numbers(3,4)
-    print(num_operations.add_num())
-    print(num_operations.sub_num())
-    print(num_operations.multi())
-    print(num_operations.divide())
+    num_operations.add_num()
+    num_operations.sub_num()
+    num_operations.multi()
+    num_operations.divide()
+    num_operations.findPrime()
